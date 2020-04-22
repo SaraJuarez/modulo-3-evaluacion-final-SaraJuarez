82242e511016281b77dc92994692ab70443eacfb
adalaber <sarajuarezzarco@gmail.com>
2020-04-22 21:19:59 +0200

    Add changes related to preventDefault

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ function App() {
     getDataFromApi().then(data => { setCharacter(data); setCharacterFilter(data) })
   }, []);
 
+
+  const preventDefault = (ev) => {
+    ev.preventDefault();
+  }
+
   const handleInput = (ev) => {
+
+    // cada vez que hago un cambio en el input, se ejecuta handleInput y se renueva el valor de characterSelect, filtrando el array character y guardando los cambios en characterFilter, que es lo que se pinta. Si yo escribo Rick, me salen los personajes que contienen esas letras(se filtra el array character guardando los resultados en characterFilter y pitando), si borro y escribo rock, vuelve a filtrar el array character y a buscar las letras que coincidan, guardando el resultado el characterFilter. Es decir, uso character como fuente de información y characterFilter como array donde se hacen los cambios y se pintan en pantalla.
     let inputText = ev.target.value;
     let charactersSelected = character.filter(character => character.name.toUpperCase().includes(inputText.toUpperCase()))
     setCharacterFilter(charactersSelected);
@@ -21,7 +28,7 @@ function App() {
 
   return (
     <div className="App">
-      <Filter inputFunction={handleInput} />
+      <Filter defaultFunction={preventDefault} inputFunction={handleInput} />
       <CharacterList characters={characterFilter} />
 
     </div>
--- src/components/filter.js
+++ src/components/filter.js
@@ -8,7 +8,7 @@ const Filter = (props) => {
 
         <div className='filter-box'>
             <img className='filter-box__logo' title='Logo' alt='Logo Rick and Morty' src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png'></img>
-            <form>
+            <form onSubmit={props.defaultFunction}>
                 <input onChange={props.inputFunction} className='filter-box__input'></input>
             </form>
 
