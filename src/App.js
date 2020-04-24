292ef82e0fc959795b3133a3452af37392f0c386
adalaber <sarajuarezzarco@gmail.com>
2020-04-24 15:09:43 +0200

    Add more styles

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import CharacterDetail from './components/characterDetail';
 function App() {
   const [character, setCharacter] = useState([]);
   const [characterFilter, setCharacterFilter] = useState([]);
+  const [inputText, setInputText] = useState('');
   useEffect(() => {
-    localStorage.setItem('search', '')
+    // localStorage.setItem('search', '')
     getDataFromApi().then(data => { setCharacter(data); setCharacterFilter(data) })
   }, []);
 
 
-  const lastSearch = localStorage.getItem('search');
+  // const lastSearch = localStorage.getItem('search');
 
   const preventDefault = (ev) => {
     ev.preventDefault();
@@ -26,7 +27,7 @@ function App() {
 
     // cada vez que hago un cambio en el input, se ejecuta handleInput y se renueva el valor de characterSelect, filtrando el array character y guardando los cambios en characterFilter, que es lo que se pinta. Si yo escribo Rick, me salen los personajes que contienen esas letras(se filtra el array character guardando los resultados en characterFilter y pitando), si borro y escribo rock, vuelve a filtrar el array character y a buscar las letras que coincidan, guardando el resultado el characterFilter. Es decir, uso character como fuente de información y characterFilter como array donde se hacen los cambios y se pintan en pantalla.
     let inputText = ev.target.value;
-    localStorage.setItem('search', inputText)
+    setInputText(inputText);
 
     let charactersSelected = character.filter(character => character.name.toUpperCase().includes(inputText.toUpperCase()))
     if (charactersSelected.length === 0) {
@@ -42,7 +43,7 @@ function App() {
     <Switch>
       <Route exact path='/' render={() =>
         <main className="App">
-          <Filter lastSearch={lastSearch} defaultFunction={preventDefault} inputFunction={handleInput} />
+          <Filter lastSearch={inputText} defaultFunction={preventDefault} inputFunction={handleInput} />
           <CharacterList characters={characterFilter} />
 
         </main>
--- src/components/characterDetail.js
+++ src/components/characterDetail.js
@@ -10,18 +10,18 @@ const CharacterDetail = (props) => {
 
     return (
         <div className='card-box'>
-            <img className='filter-box__logo' title='Logo' alt='Logo Rick and Morty' src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png'></img>
+            <img className='card-box__logo' title='Logo' alt='Logo Rick and Morty' src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png'></img>
             <Link className='card-box__link-return' to={`/`}> > Volver</Link>
 
             <div className='card-box__infoCharacter'>
-                <div className='card-box__infoCharacter--image'>
-                    <img title={characterDetail.name} alt={`imagen de + ${characterDetail.name}`} src={characterDetail.image} className='wrap__character--image' ></img>
+                <div >
+                    <img title={characterDetail.name} alt={`imagen de + ${characterDetail.name}`} src={characterDetail.image} className='detail-image' ></img>
                 </div>
                 <div className='card-box__infoCharacter--text'>
                     <p>Nombre: {characterDetail.name} <i className={characterDetail.alive === 'Alive' ? 'fas fa-heartbeat' : 'fas fa-skull-crossbones'}></i></p>
-                    <p>Especie: <img className='woman' src={characterDetail.species === 'Human' ? human : miscellaneous}></img></p>
+                    <p >Especie: <img className='woman' src={characterDetail.species === 'Human' ? human : miscellaneous}></img></p>
                     <p>Origen: {characterDetail.planet}</p>
-                    <p className='infoCharacter__text--episodes'>Episodios: {characterDetail.episodes}</p>
+                    <p>Episodios: {characterDetail.episodes}</p>
 
 
                 </div>
